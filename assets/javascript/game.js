899425a2a8086d9879e72f1f4fa9e36fe54db8ea
sunbas2 <sunbas777@gmail.com>
2018-04-25 13:21:35 -0400

    added new file

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+var wins = 0;
+var losses = 0;
+var nGuess = 15;
+var cGuess = [];
+
+document.onkeyup = function (event) {
+
+    var userGuess = event.key;
+
+    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoices.length)];
+
+    var selection = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+    
+    if (selection.indexOf(userGuess) > -1) {
+
+        if (userGuess === computerGuess) {
+            wins++;
+            nGuess = 15;
+            cGuess = [];
+        }
+
+        if (userGuess != computerGuess) {
+            nGuess --;
+            cGuess.push(userGuess);
+        }
+
+        if (nGuess === 0) {
+            nGuess = 15;
+            losses ++;
+            cGuess = [];
+        }
+
+        var winSS = document.getElementById("wins");
+        var lossS = document.getElementById("losses");
+        var nuGue = document.getElementById("guessleft");
+        var cGues = document.getElementById("yourguesses");
+
+        document.getElementById("container").innerHTML;
+
+        
+
+    }
+};
